a7e4dc83cb6e5a8b46c257321756decc225545f9
Alex Sharp <ajsharp@gmail.com>
2009-07-23 11:35:07 -0500

    updated post model and unit tests to ensure permalink is populated when a new model is saved

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title, :body
   validates_uniqueness_of :title
+
+  before_save :generate_permalink
   
-  def permalink
-    title.gsub(" ", "_").downcase
-  end
+  protected
+    def generate_permalink
+      #attributes[:permalink] = title.gsub(" ", "_").downcase
+      write_attribute(:permalink, title.gsub(" ", "_").downcase)
+    end
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -10,6 +10,14 @@ class PostTest < ActiveSupport::TestCase
       should_have_db_columns :title, :body, :permalink
       should_validate_presence_of :title, :body
       should_validate_uniqueness_of :title
+
+      context "before saving" do
+        should "create a permalink based on the name" do
+          new_post = Post.create(:title => "New title", :body => "...")
+          assert_valid new_post
+          assert_not_nil new_post.permalink
+        end
+      end # end of before saving
     end # end of shoulda macros
     
     context "instance methods" do
